5ed57ebe10b1113b442ed1688d4ccb36485a635b
Daniel Emery <daniel@sine.co>
2020-01-21 16:16:36 +1030

    Removed build artifacts from repo

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { Configuration, Target } from './configuration';
-import { Season } from './season';
-import { Team } from './team';
-import { Match } from './match';
-export { Configuration, Target, Season, Team, Match };
--- dist/team.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * A volleyball team.
- */
-export declare class Team {
-    /** The name of the team. */
-    Name: string;
-}
