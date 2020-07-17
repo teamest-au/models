dda252c39ef9c6e9014efd59fe774ff97c0d3a23
Daniel Emery <daniel@sine.co>
2020-07-17 15:42:48 +0930

    Added isExternal flag to team model

--- src/raw/team.ts
+++ src/raw/team.ts
@@ -4,9 +4,13 @@
 export interface Team {
   /** The name of the team. */
   name: string;
+  /** Whether the team is external to this grade/competition. */
+  isExternal: boolean;
 }
 
 export interface SerialisedTeam {
   /** The name of the team. */
   name: string;
+  /** Whether the team is external to this grade/competition. */
+  isExternal: boolean;
 }
